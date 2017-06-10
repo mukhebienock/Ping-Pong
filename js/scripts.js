dd45b1e1b1bc0afea6292cafb59cfeab6157190f
Enock Mukhebi <mukhebienock@gmail.com>
2017-06-11 00:09:50 +0300

    add the third condition

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,9 @@ var pingPongNumber = [];
 
 var pingpong = function(inputtedNumber) {
   for(var index = 1; index <= inputtedNumber; index += 1) {
-    if(index % 3 === 0){
+    if((index % 3 === 0) && (index % 5 === 0)){
+      pingPongNumber.push("pingpong");
+    } else if (index % 3 === 0){
       pingPongNumber.push("ping");
     } else if (index % 5 === 0){
       pingPongNumber.push("pong");
